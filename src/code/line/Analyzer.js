ba8a24a0912243ddc9d318f84603bf1154711cf6
Varun Jalandery <varun.j@circles.asia>
2018-07-01 11:28:15 +0800

    dev Add getModuleReferenceName() implementation

--- src/code/line/Analyzer.js
+++ src/code/line/Analyzer.js
@@ -3,8 +3,23 @@ class Analyzer {
         return (new RegExp(`require(.)+${moduleName}`)).test(line);
     }
 
-    getModuleReferenceName(line, moduleName) {
-        return '';
+    getModuleReferenceName(line) {
+        const initialMatches = line.match(new RegExp('(var|const)\\s+[a-zA-z]+(\\s?|\\s+)\\='));
+        if (!initialMatches || !initialMatches[0]) {
+            return '';
+        }
+
+        const beforeEqualToSignMatch = initialMatches[0].match(new RegExp('\\s+[a-zA-z]+(\\=|(\\s?))'));
+        if (!beforeEqualToSignMatch || !beforeEqualToSignMatch[0]) {
+            return '';
+        }
+
+        const moduleReferenceNameMatch = beforeEqualToSignMatch[0].match(new RegExp('[a-zA-Z]+'));
+
+        if (!moduleReferenceNameMatch || !moduleReferenceNameMatch[0]) {
+            return '';
+        }
+        return moduleReferenceNameMatch[0];
     }
 }
 module.exports = Analyzer;
